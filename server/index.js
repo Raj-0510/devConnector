4c5e9cb33626ccff7e8151d5afe8affbfa8e34b5
Raj Jayswal <rajjayswal480@gmail.com>
2025-07-18 13:40:14 +0530

    MongoURL edit

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const profileRoute = require("./routes/userProfile");
 const allProfiles = require("./routes/allProfiles");
 const feedRoutes = require("./routes/feedRoutes");
 
-mg.connect("mongodb://localhost:27017/devConnector")
+mg.connect("mongodb+srv://rajjayswal480:raj5102003>@cluster0.d09rsaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("connected with db");
   })
